0686e7f7cba540e7b80f9f289c418dad8e7bf73b
qqq901 <99828558+qqq901@users.noreply.github.com>
2022-02-17 21:06:11 +0300

    Update App.js

--- 4/src/App.js
+++ 4/src/App.js
@@ -249,7 +249,7 @@ function App() {
             >
               <StyledButton
                 onClick={(e) => {
-                  window.open("/config/roadmap.pdf", "_blank");
+                  window.open("/config/roadmap.jpg", "_blank");
                 }}
                 style={{
                   margin: "5px",
